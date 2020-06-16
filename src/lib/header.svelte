5bf82b621f6d02b6a9a6fa728be24c5b96e9068c
Yazid <secho771@gmail.com>
2020-06-16 19:34:27 +0100

    removed autofocus

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -9,7 +9,7 @@
 <header class="bg-white shadow h-16 px-4 fixed w-full flex justify-between items-center">
     <h1 class="hidden md:inline text-xl mr-8 text-blue-600">codename</h1>
     <div class="flex flex-grow">
-        <input class="text-blue-600 p-2 rounded-lg text-indent w-full" type="text" placeholder="Translate i.e. Night" bind:value on:keydown={e => e.which === 13 ? translate() : ''} autofocus>
+        <input class="text-blue-600 p-2 rounded-lg text-indent w-full" type="text" placeholder="Translate i.e. Night" bind:value on:keydown={e => e.which === 13 ? translate() : ''}>
         <button class="hover:bg-blue-600 hover:text-white text-blue-600 bg-white p-2 h-full w-10 rounded-full ml-4" on:click={() => translate() }><i class="fas fa-arrow-right"></i></button>
     </div>
 </header>
